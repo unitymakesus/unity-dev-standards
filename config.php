4df5873ee3900846043b501c9f3d8573e10adebf
Andy Marino <andy@unitywebagency.com>
2020-11-03 19:36:00 -0500

    setup navigation logic and templates

--- config.php
+++ config.php
@@ -1,8 +1,28 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'baseUrl'     => '',
     'title'       => 'Unity Web Agency',
     'description' => 'Website description.',
     'production'  => false,
+
+    // navigation menu
+    'navigation' => require_once('lib/nav.php'),
+
+    // helpers
+    'isActive' => function ($page, $path) {
+        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
+    },
+    'isActiveParent' => function ($page, $menuItem) {
+        if (is_object($menuItem) && $menuItem->children) {
+            return $menuItem->children->contains(function ($child) use ($page) {
+                return trimPath($page->getPath()) == trimPath($child);
+            });
+        }
+    },
+    'url' => function ($page, $path) {
+        return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
+    },
 ];
