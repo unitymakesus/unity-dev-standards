d7ad56906f967e984797deb9ef3a3823ed676d8d
Andy Marino <andy@unitywebagency.com>
2020-11-03 19:44:42 -0500

    setup partials, styles, and scripts for highlighting

--- source/_partials/head.blade.php
+++ source/_partials/head.blade.php
@@ -2,7 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="dns-prefetch" href="//fonts.googleapis.com" />
-  <title>{{ $page->title }}</title>
+  <title>{{ $page->siteName }}{{ $page->title ? ' | ' . $page->title : '' }}</title>
+  <meta name="robots" content="noindex,nofollow">
+  <link rel="icon" href="/assets/images/favicon.png">
+  <link href="https://fonts.googleapis.com/css2?family=Chivo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 </head>
